5b61ae9a91e40066aedab34dbfbee766f90d33e0
Iulian Preda <iulian.octavian.preda@gmail.com>
2020-10-10 22:06:08 +0300

    Added decimal.js

--- frontend/monte-carlo-visualizer-react/src/models/distribution.model.ts
+++ frontend/monte-carlo-visualizer-react/src/models/distribution.model.ts
@@ -1,6 +1,12 @@
+import { Decimal } from 'decimal.js';
+
 import { Point } from './point.model';
 
 export interface Distribution {
     points: Point[];
     elapsedTime: number;
+
+    approximatedPi: Decimal;
+
+    elapsedTimeApproximatingPi: number;
 }
